1a4433354cb9823946e2a73f12fe1ef91a6cc2c3
saci zakaria <sacideveloppeur@gmail.com>
2021-09-08 11:29:16 +0200

    obscureText

--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -27,10 +27,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 color: Colors.lime,
                 child: TextField(
                   decoration: InputDecoration(
-                    icon: Icon(Icons.mail_outline,size: 30,color: Colors.black,),
+                      icon: Icon(
+                        Icons.mail_outline, size: 30, color: Colors.black,),
                       labelText: "Adress Mail",
                       labelStyle: TextStyle(
-                        fontSize: 20,
+                          fontSize: 20,
                           color: Colors.black
                       ),
                       hintText: "Enter eMail",
@@ -49,7 +50,56 @@ class _MyHomePageState extends State<MyHomePage> {
                 color: Colors.orange,
                 child: TextField(
                   decoration: InputDecoration(
-                      icon: Icon(Icons.password,size: 30,color: Colors.black,),
+                      icon: Icon(
+                        Icons.password, size: 30, color: Colors.black,),
+                      labelText: "Passeword",
+                      labelStyle: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black
+                      ),
+                      hintText: "Enter Passeword",
+                      hintStyle: TextStyle(
+                        fontSize: 15,
+                      )
+                  ),
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText:true,
+
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                margin: EdgeInsets.all(20.0),
+                color: Colors.lime,
+                child: TextField(
+                  decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.mail_outline, size: 30, color: Colors.black,),
+                      labelText: "Adress Mail",
+                      labelStyle: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black
+                      ),
+                      hintText: "Enter eMail",
+                      hintStyle: TextStyle(
+                        fontSize: 15,
+                      )
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                margin: EdgeInsets.all(20.0),
+                color: Colors.orange,
+                child: TextField(
+                  decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.password, size: 30, color: Colors.black,),
                       labelText: "Passeword",
                       labelStyle: TextStyle(
                           fontSize: 20,
@@ -71,7 +121,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 color: Colors.lime,
                 child: TextField(
                   decoration: InputDecoration(
-                      icon: Icon(Icons.mail_outline,size: 30,color: Colors.black,),
+                      icon: Icon(
+                        Icons.mail_outline, size: 30, color: Colors.black,),
                       labelText: "Adress Mail",
                       labelStyle: TextStyle(
                           fontSize: 20,
@@ -93,51 +144,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 color: Colors.orange,
                 child: TextField(
                   decoration: InputDecoration(
-                      icon: Icon(Icons.password,size: 30,color: Colors.black,),
-                      labelText: "Passeword",
-                      labelStyle: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black
-                      ),
-                      hintText: "Enter Passeword",
-                      hintStyle: TextStyle(
-                        fontSize: 15,
-                      )
-                  ),
-                  keyboardType: TextInputType.visiblePassword,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                margin: EdgeInsets.all(20.0),
-                color: Colors.lime,
-                child: TextField(
-                  decoration: InputDecoration(
-                      icon: Icon(Icons.mail_outline,size: 30,color: Colors.black,),
-                      labelText: "Adress Mail",
-                      labelStyle: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black
-                      ),
-                      hintText: "Enter eMail",
-                      hintStyle: TextStyle(
-                        fontSize: 15,
-                      )
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                margin: EdgeInsets.all(20.0),
-                color: Colors.orange,
-                child: TextField(
-                  decoration: InputDecoration(
-                      icon: Icon(Icons.password,size: 30,color: Colors.black,),
+                      icon: Icon(
+                        Icons.password, size: 30, color: Colors.black,),
                       labelText: "Passeword",
                       labelStyle: TextStyle(
                           fontSize: 20,
